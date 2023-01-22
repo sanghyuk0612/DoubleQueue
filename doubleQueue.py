966e5fedf35e09fd4aadb729e2787f4934f2103f
sanghyuk0612 <hwhwhw944@gmail.com>
2023-01-22 14:49:13 +0900

    Update doubleQueue.py

--- doubleQueue.py
+++ doubleQueue.py
@@ -3,6 +3,7 @@ answer = []
 i = 0
 
 while i<len(operations):
+    
     if operations[i][0] == "I":
        j = 2
        string = ''
@@ -10,7 +11,19 @@ while i<len(operations):
            string += operations[i][j]
            j +=1
        answer.append(int(string))
-    
+    else:
+        if len(answer)==0:
+            pass
+        
+        elif operations[i][2] == "-":
+            answer.remove(min(answer))
+        else:
+            answer.remove(max(answer))
     i+=1
+if len(answer)==0:
+    print("0 , 0")
+else:
+    print(min(answer))
+    print(max(answer))
     
 
